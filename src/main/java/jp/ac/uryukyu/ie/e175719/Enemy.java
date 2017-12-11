17e480d36b984b85c3c6a79589f734e5d9b1c1f2
Yuiri Arakaki <e175719@ie.u-ryukyu.ac.jp>
2017-12-11 16:55:25 +0900

    step3 commit

--- src/main/java/jp/ac/uryukyu/ie/e175719/Enemy.java
+++ src/main/java/jp/ac/uryukyu/ie/e175719/Enemy.java
@@ -8,42 +8,66 @@ package jp.ac.uryukyu.ie.e175719;
  *  boolean dead; //敵の生死状態。true=死亡。
  * Created by tnal on 2016/11/13.
  */
-public class Enemy extends LivingThing{
+public class Enemy extends LivingThing {
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
-     * @param name モンスター名
+     *
+     * @param name      モンスター名
      * @param maximumHP モンスターのHP
-     * @param attack モンスターの攻撃力
+     * @param attack    モンスターの攻撃力
      */
-    public Enemy (String name, int maximumHP, int attack) {
-        super(name,maximumHP,attack);
+    public Enemy(String name, int maximumHP, int attack) {
+        super(name, maximumHP, attack);
     }
-
 
 
     /**
      * 自身へ攻撃されたときのダメージ処理をするメソッド。
      * 指定されたダメージを hitPoint から引き、死亡判定を行う。
+     *
      * @param damage 受けたダメージ
-     * hp = hitPoint
-     * na = name
-     * de = dead
+     *               hp = hitPoint
+     *               na = name
+     *               de = dead
      */
 
     @Override
-    public void wounded(int damage){
+    public void wounded(int damage) {
         int hp = getHitPoint();
         String na = getName();
         boolean de = getDead();
 
         hp -= damage;
         setHitPoint(hp);
-        if( hp < 0 ) {
+        if (hp < 0) {
             de = true;
             setDead(de);
             System.out.printf("モンスター%sは倒れた。\n", na);
         }
+    }
+
+    @Override
+    public void attack(LivingThing opponent){
+        boolean de = getDead();
+        String na = getName();
+        int at = getAttack();
+        if(de == false) {
+            int damage = (int) (Math.random() * at);
+            int probability = (int) (Math.random() * 10);
+            if(damage == 0 ) {
+                System.out.printf("%sの攻撃！,,,だが、%sは攻撃を回避した！\n", na, opponent.getName());
+            }else{
+                if( probability <= 3 ) {
+                    System.out.printf("%sの攻撃！痛恨の一撃！！%sに%dのダメージを与えた！！\n", na, opponent.getName(), damage * 2);
+                    opponent.wounded(damage);
+
+                }else{
+                    System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", na, opponent.getName(), damage);
+                }
+            }
+        }
+
     }
 
 }
--- src/main/java/jp/ac/uryukyu/ie/e175719/Hero.java
+++ src/main/java/jp/ac/uryukyu/ie/e175719/Hero.java
@@ -30,4 +30,27 @@ public class Hero extends LivingThing {
             System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", na);
         }
     }
+    @Override
+    public void attack(LivingThing opponent){
+        boolean de = getDead();
+        String na = getName();
+        int at = getAttack();
+        if(de == false) {
+            int damage = (int) (Math.random() * at);
+            int probability = (int) (Math.random() * 10);
+            if(damage == 0 ) {
+                System.out.printf("%sの攻撃！,,,だが、%sは攻撃を回避した！\n", na, opponent.getName());
+            }else{
+                if( probability <= 4 ) {
+                    System.out.printf("%sの攻撃！会心の一撃！！%sに%dのダメージを与えた！！\n", na, opponent.getName(), damage * 2);
+                    opponent.wounded(damage);
+
+                }else{
+                    System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", na, opponent.getName(), damage);
+                }
+            }
+        }
+
+    }
+
 }
